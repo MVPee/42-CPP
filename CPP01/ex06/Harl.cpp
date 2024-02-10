d4a5a2607e14ec6427719011a843e88ad440983f
mvpee <mariusvanpee@hotmail.be>
2024-02-10 17:59:26 +0100

    add \n

--- CPP01/ex06/Harl.cpp
+++ CPP01/ex06/Harl.cpp
@@ -6,22 +6,22 @@ Harl::~Harl() { return ; }
 
 void Harl::debug( void )
 {
-	std::cout << "[ DEBUG ]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.\nI really do!" << std::endl;
+	std::cout << "[ DEBUG ]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.\nI really do!\n" << std::endl;
 }
 
 void Harl::info( void )
 {
-	std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger!\nIf you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger!\nIf you did, I wouldn’t be asking for more!\n" << std::endl;
 }
 
 void Harl::warning( void )
 {
-	std::cout << "[ WARNING ]\nI think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "[ WARNING ]\nI think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month.\n" << std::endl;
 }
 
 void Harl::error( void )
 {
-	std::cout << "[ ERROR ]\nThis is unacceptable,\nI want to speak to the manager now." << std::endl;
+	std::cout << "[ ERROR ]\nThis is unacceptable,\nI want to speak to the manager now.\n" << std::endl;
 }
 
 int Harl::getLevel(std::string level) {
