cbe834d62b2758c473c6d671d92464d2b2975552
MVPee <mariusvanpee@hotmail.be>
2024-03-25 16:44:36 +0100

    littl fix

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,17 +1,22 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
-int main(int ac, char **av) {
-  if (ac == 1)
-    return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 1);
-
-  for (int i = 1; i < ac; i++) {
-    for (int j = 0; av[i][j] != '\0'; j++) {
-      std::cout << (char)toupper(av[i][j]);
-    }
-    if (i < ac -1)
-      std::cout << " ";
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+  {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return (1);
   }
-
-  return (std::cout << "" << std::endl, 0);
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j] != '\0'; j++)
+		{
+			std::cout << (char)toupper(av[i][j]);
+		}
+		if (i < ac - 1)
+			std::cout << " ";
+	}
+	std::cout << "" << std::endl;
+  return (0);
 }
