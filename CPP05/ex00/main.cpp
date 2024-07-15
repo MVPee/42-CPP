6784fae9371bc62f8b557ce64898a9572c996cad
MVPee <mariusvanpee@hotmail.be>
2024-07-15 14:47:26 +0200

    fix somes try catch

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -1,38 +1,48 @@
 #include "Bureaucrat.hpp"
 
 int main(void) {
+    std::cout << std::endl;
+    std::cout << "PART 1." << std::endl;
     try {
         Bureaucrat test("Low", 151);
-    } catch (std::exception & e) {
+    }
+    catch (std::exception & e) {
         std::cerr << ORANGE << e.what() << NONE <<std::endl;
     }
-
     try {
         Bureaucrat test1("Hight", 0);
-    } catch (std::exception & e) {
+    }
+    catch (std::exception & e) {
         std::cerr << ORANGE << e.what() << NONE <<std::endl;
     }
     
-    Bureaucrat test2("increment", 1);
+    std::cout << std::endl;
+    std::cout << "PART 2." << std::endl;
     try {
+        Bureaucrat test2("increment", 0);
         test2.increment();
-    } catch (std::exception & e) {
+    }
+    catch (std::exception & e) {
         std::cerr << ORANGE << e.what() << NONE <<std::endl;
     }
-
-    Bureaucrat test3("decrement", 150);
     try {
+        Bureaucrat test3("decrement", 150);
         test3.decrement();
-    } catch (std::exception & e) {
+    }
+    catch (std::exception & e) {
         std::cerr << ORANGE << e.what() << NONE <<std::endl;
     }
 
-    Bureaucrat MVP("MVP", 2);
+    std::cout << std::endl;
+    std::cout << "PART 3." << std::endl;
     try {
+        Bureaucrat MVP("MVP", 2);
         MVP.increment();
-    } catch (std::exception & e) {
+        std::cout << ORANGE << MVP << NONE << std::endl;
+    }
+    catch (std::exception & e) {
         std::cerr << ORANGE << e.what() << NONE <<std::endl;
     }
-    std::cout << ORANGE << MVP << NONE << std::endl;
+
     return (0);
 }
