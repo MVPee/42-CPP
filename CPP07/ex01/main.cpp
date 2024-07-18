501ef8ecb4ce78dd98d0a3d2c25d79aca40e0a81
MVPee <mariusvanpee@hotmail.be>
2024-07-18 16:56:58 +0200

    add test

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -1,16 +1,25 @@
 # include "iter.hpp"
 
-# define SIZE 10
+# define SIZE 3
 
 template <typename T>
-static void print(T value) {
+void print(T value) {
     std::cout << "value: " << value << std::endl;
+}
+
+template <typename T>
+void increment(T &value) {
+    value++;
 }
 
 int main(void) {
     int *int_array = new int[SIZE];
     for (size_t i = 0; i < SIZE; i++)
         int_array[i] = i * 3 % 2 + i;
+
+    ::iter(int_array, SIZE, print<int>);
+    ::iter(int_array, SIZE, increment<int>);
+    std::cout << std::endl;
 
     ::iter(int_array, SIZE, print<int>);
 
