01f3c49d92b648fc79ec4db61ef8d8c3668a6942
Marius Van Pee <mariusvanpee@hotmail.be>
2024-09-09 16:44:04 +0200

    cpp08 ex01

---
+++ CPP08/ex01/Span.cpp
@@ -0,0 +1,83 @@
+#include "Span.hpp"
+
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+Span::Span() {
+}
+Span::Span(unsigned int size) : _size(size){}
+
+Span::Span(const Span &src) {
+	_size = src._size;
+	_list = src._list;
+}
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+Span::~Span() {
+}
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+Span &Span::operator=(const Span &rhs) {
+	if ( this != &rhs )
+		_list = rhs._list;
+	return *this;
+}
+
+std::ostream &operator<<(std::ostream &o, const Span &i) {
+	(void)i;
+	return o;
+}
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+void Span::addNumber(int number) {
+	if (_list.size() < _size)
+		_list.push_back(number);
+	else
+		throw std::length_error("Span is full...");
+};
+
+int Span::shortestSpan(void) {
+    if (_list.size() < 2)
+        throw std::length_error("Span needs at least two elements for this function...");
+
+    std::list<int> copy = _list;
+    std::list<int>::const_iterator start;
+    std::list<int>::const_iterator next;
+
+    copy.sort();
+    start = copy.begin();
+    next = ++copy.begin();
+    int shortest = *next - *start;
+    while (next != copy.end()) {
+        ++start;
+        ++next;
+        if (next != copy.end() && (*next - *start) < shortest)
+            shortest = *next - *start;
+    }
+
+    return (shortest);
+}
+
+int Span::longestSpan(void) {
+	if (_list.size() < 2)
+		throw std::length_error("Span need two elements for this function...");
+	std::list<int>::const_iterator start = _list.begin();
+    std::list<int>::const_iterator end = _list.end();
+	return (*std::max_element(start, end) - *std::min_element(start, end));
+}
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+/* ************************************************************************** */
---
+++ CPP08/ex01/Span.hpp
@@ -0,0 +1,27 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+# include <iostream>
+# include <string>
+# include <list>
+
+class Span {
+	private:
+		std::list<int>	_list;
+		unsigned int	_size;
+
+		Span();
+	public:
+		Span(unsigned int size);
+		Span(const Span &src);
+		Span &operator=(const Span &rhs);
+		~Span();
+
+		void addNumber(int number);
+		int shortestSpan(void);
+		int longestSpan(void);
+};
+
+std::ostream &operator<<(std::ostream &o, const Span &i);
+
+#endif /* ************************************************************ SPAN_H */
